d032c8676238d151e8496d5a5478e2efb53b85bd
Clay Coleman <jclaytoncoleman@gmail.com>
2019-04-18 00:28:07 -0600

    Remove unneccessary code

--- server/patients.js
+++ server/patients.js
@@ -3,15 +3,6 @@ const express = require('express');
 const router = express.Router();
 const auth = require('./auth.js');
 
-// Configure multer so that it will upload to '/public/images'
-const multer = require('multer');
-const upload = multer({
-  dest: '../public/images/',
-  limits: {
-    fileSize: 10000000,
-  },
-});
-
 const users = require('./users.js');
 const User = users.model;
 
